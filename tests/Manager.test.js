ae2254c10f1b569ac7a3a8cad51fd8b678fc993c
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-05 13:03:50 -0500

    tests all pass for manager

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,3 +1,4 @@
+
 const Manager = require('../Manager')
 const manager = new Manager("Bruce Wayne", "002", "notBatman@gmail.com", "1000")
 
@@ -6,6 +7,30 @@ describe("manager", () => {
         expect(manager.name).toBe("Bruce Wayne");
         expect(manager.id).toBe("002");
         expect(manager.email).toBe("notBatman@gmail.com");
-        EXPORTDECLARATION_TYPES(manager.officeNumber).toBe("1000")
+        expect(manager.officeNumber).toBe("1000")
     }) 
+})
+
+describe ("getName method", () => {
+    it("should return getName method", ()=> {
+        expect(manager.getName()).toBe("Bruce Wayne")
+    })
+})
+
+describe ("getid method", () => {
+    it("should return getId method", ()=> {
+        expect(manager.getId()).toBe("002")
+    })
+})
+
+describe ("getEmail method", () => {
+    it("should return getEmail method", ()=> {
+        expect(manager.getEmail()).toBe("notBatman@gmail.com")
+    })
+})
+
+describe ("getofficeNumber method", () => {
+    it("should return getOfficeNumber method", ()=> {
+        expect(manager.getOfficeNumber()).toBe("1000")
+    })
 })
