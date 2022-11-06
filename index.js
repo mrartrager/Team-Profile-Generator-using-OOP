23ef7460653c266c97b2c2ac431a2039942f93d9
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-06 10:09:20 -0600

    added questions for manager

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const Manager = require("./library/Manager");
-const Egineer = require("./library/Egineer");
+const Engineer = require("./library/Engineer");
 const Intern = require("./library/Intern");
-const htmlPage = require("./generateProfiles"); // link html to page
+const htmlPage = require("./generateProfiles"); 
 const Employee = require("./library/Employee");
 
 
 let teamMembers = []
 
+const managerPrompt = () => {
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "Name",
+          message: "What is the managers name?",
+        },
+        {
+          type: "input",
+          name: "ID",
+          message: "What is the managers ID?",
+        },
+        {
+          type: "input",
+          name: "Email",
+          message: "What is the managers email?",
+        },
+        {
+          type: "input",
+          name: "Office Number",
+          message: "What is the managers office number?",
+        },
+    ])
+    .then((answers) => {
+        employeeAnswer.push(answers);
+        nextEmployeePrompt();
+    })
+    .catch((error) => console.log(error));
+};
+
+managerPrompt();
