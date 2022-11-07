d45eb62f7e3ed5f853a7fb1b118cdb9cb1f8a3b9
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-07 17:47:09 -0600

    page layout built in generateProfiles_added html to own folder

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Engineer = require("./library/Engineer");
 const Intern = require("./library/Intern");
 // const Employee = require("./library/Employee");
 
-// const generateHTML = require("./src/index.html") 
+// const generateHTML = require("./src/generateProfiles") 
 
 let employeeAnswer = []; 
 
--- src/gernerateProfiles.js
+++ src/gernerateProfiles.js
@@ -1,9 +1,66 @@
+const generateHTML = (team) => {
+  const profiles = []
 
+  managerCard = (manager) => {
+    return `
+    <div class="hover:scale-105 hover:border-slate-500 hover:bg-gradient-to-b from-slate-100 to-slate-400 box-border h-max w-max p-4 border-4 m-10 p-10 border-gray-300 rounded-3xl drop-shadow-lg">
+    <div class="">
+      <h1 class="flex flex-col justify-center items-center font-bold text-6xl text-slate-700 pb-4 mb-4"> ${manager.name} </h1>
+       <h2 class="flex flex-col justify-center items-center font-bold text-6xl text-blue-800">Manager</h2>
+    </div>
+    <h2 class="hover:bg-blue-800 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">ID:<span>${manager.id}</span></h2>
+    <h2 class="hover:bg-blue-800 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Email:<span>${manager.email}</span></h2>
+    <h2 class="hover:bg-blue-800 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Office Number:<span>${manager.officeNumber}</span></h2>
+  </div>
+    
+    `
+  }
 
-function genenerateteam(){
- 
+  engineerCard = (engineer) => {
+    return `
+    <div class="hover:scale-105 hover:border-slate-500 hover:bg-gradient-to-b from-slate-100 to-slate-400 box-border h-max w-max p-4 border-4 m-10 p-10 border-gray-300 rounded-3xl drop-shadow-lg">
+    <div class="">
+      <h1 class="flex flex-col justify-center items-center font-bold text-6xl text-slate-700 pb-4 mb-4">${engineer.name}</h1>
+       <h2 class="flex flex-col justify-center items-center font-bold text-6xl text-teal-700">Engineer</h2>
+    </div>
+    <h2 class="hover:bg-teal-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">ID:<span>${engineer.id}</span></h2>
+    <h2 class="hover:bg-teal-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Email:<span>${engineer.email}</span></h2>
+    <h2 class="hover:bg-teal-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Github<span>${engineer.github}</span></h2>
+  </div>
+    `
+
+  }
+
+internCard = (intern) => {
+return `
+<div class="hover:scale-105 hover:border-slate-500 hover:bg-gradient-to-b from-slate-100 to-slate-400 box-border h-max w-max p-4 border-4 m-10 p-10 border-gray-300 rounded-3xl drop-shadow-lg">
+<div class="">
+  <h1 class="flex flex-col justify-center items-center font-bold text-6xl text-slate-700 pb-4 mb-4">${intern.name}</h1>
+   <h2 class="flex flex-col justify-center items-center font-bold text-6xl text-green-700">Intern</h2>
+</div>
+<h2 class="hover:bg-green-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">ID:<span>${intern.id}</span></h2>
+<h2 class="hover:bg-green-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Email:<span>${intern.email}</span></h2>
+<h2 class="hover:bg-green-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">School:<span>${intern.university}</span></h2>
+</div>
+`
+
+}
+team.forEach(function(teamMember) {
+  if (teamMember.getRole()=== "Manager" ) {
+    profiles.push(managerCard(teamMember))
+  } else if (teamMember.getRole()=== "Engineer" ) {
+    profiles.push(engineer(teamMember))
+  } else if (teamMember.getRole()=== "Intern" ) {
+    profiles.push(intern(teamMember))
+  }
+})
+
+return profiles.join('')
+}
+module.exports = (team) => {
+
   return `
-  <!DOCTYPE html>
+  !DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -23,39 +80,13 @@ function genenerateteam(){
 
   <main class="flex flex-row justify-center items-center">
 
-    <div class="hover:scale-105 hover:border-slate-500 hover:bg-gradient-to-b from-slate-100 to-slate-400 box-border h-max w-max p-4 border-4 m-10 p-10 border-gray-300 rounded-3xl drop-shadow-lg">
-      <div class="">
-        <h1 class="flex flex-col justify-center items-center font-bold text-6xl text-slate-700 pb-4 mb-4"></h1>
-         <h2 class="flex flex-col justify-center items-center font-bold text-6xl text-blue-800">Manager</h2>
-      </div>
-      <h2 class="hover:bg-blue-800 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">ID:<span>${}</span></h2>
-      <h2 class="hover:bg-blue-800 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Email:<span></span></h2>
-      <h2 class="hover:bg-blue-800 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Office Number:<span></span></h2>
+    <div>
+    ${generateHTML(team)}
     </div>
-
-    <div class="hover:scale-105 hover:border-slate-500 hover:bg-gradient-to-b from-slate-100 to-slate-400 box-border h-max w-max p-4 border-4 m-10 p-10 border-gray-300 rounded-3xl drop-shadow-lg">
-        <div class="">
-          <h1 class="flex flex-col justify-center items-center font-bold text-6xl text-slate-700 pb-4 mb-4"></h1>
-           <h2 class="flex flex-col justify-center items-center font-bold text-6xl text-teal-700">Engineer</h2>
-        </div>
-        <h2 class="hover:bg-teal-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">ID:<span></span></h2>
-        <h2 class="hover:bg-teal-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Email:<span></span></h2>
-        <h2 class="hover:bg-teal-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Github<span></span></h2>
-      </div>
-
-      <div class="hover:scale-105 hover:border-slate-500 hover:bg-gradient-to-b from-slate-100 to-slate-400 box-border h-max w-max p-4 border-4 m-10 p-10 border-gray-300 rounded-3xl drop-shadow-lg">
-        <div class="">
-          <h1 class="flex flex-col justify-center items-center font-bold text-6xl text-slate-700 pb-4 mb-4"></h1>
-           <h2 class="flex flex-col justify-center items-center font-bold text-6xl text-green-700">Intern</h2>
-        </div>
-        <h2 class="hover:bg-green-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">ID:<span></span></h2>
-        <h2 class="hover:bg-green-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">Email:<span></span></h2>
-        <h2 class="hover:bg-green-700 box-border h-16 w-72 p-1 m-5 border-2 items-center font-semibold border-black rounded-xl">School:<span></span></h2>
-      </div>
   
   </main>
 </body>
 </html>
+  
   `
-
 }
